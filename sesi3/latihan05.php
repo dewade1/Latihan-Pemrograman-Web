a27c01d4cafe5fb0139a06669b1e0370652e8d56
dewade <127913642+dewade1@users.noreply.github.com>
2023-03-29 12:15:59 +0800

    Add files via upload

---
+++ sesi3/latihan05.php
@@ -0,0 +1,13 @@
+<?php
+    //model1
+    $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+    //print_r($age);
+
+    //echo "Umur dari Peter adalah ".$age["Peter"];
+    foreach($age as $p => $u){
+
+        echo $p . " umurnya " . $u;
+
+        echo "<br>";
+    }
